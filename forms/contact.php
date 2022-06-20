c8edd935e556fe21fc37defae1091f0d3c1b5bd2
Godgoodness@14092001 <soyebola@atbtechsoft.com>
2022-06-20 20:29:18 +0100

    ...

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,33 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+ $name = $_POST['name']
+ $email = $_POST['email']
+ $mes = $_POST['message']
+ $subject = $_POST['subject']
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$message = "<html>
+<head>
+<title> Contact Form Response </title>
+</head>
 
-  echo $contact->send();
+<body>
+<h1> New Response from your Portfolio Site </h1>
+<i>".$subject."
+<p>Name -> ".$name." </p>
+<p>Sender Mail -> ".$email."</p>
+<p>".$mes."</p>
+</body>
+</html>";
+
+if(mail('simeonoyekunleoyebola@gmail.com', $subject, $message, $headers)){
+  echo "<script>location.href='../index.html'</script>"
+} else{
+  echo "Failed to send email. Please try again"
+}
+
 ?>
